1c6b391e32479a14a7672bb9e6d4c2c29ce7d256
unknown <nedovic.vuk@gmail.com>
2023-07-23 13:26:58 +0200

    first commit

---
+++ JavaNetBeans/Java1/src/main/java/com/mycompany/java1/Java1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java1;
+
+/**
+ *
+ * @author Ceda
+ */
+public class Java1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
