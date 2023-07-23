725aae35bd21474d238fb28e03b2afa9ec378082
unknown <nedovic.vuk@gmail.com>
2023-07-23 14:37:29 +0200

    going over int, double, boolean, char, String, sout, commenting

---
+++ prva vezba/osnove.java
@@ -0,0 +1,27 @@
+public class osnove {
+    public static void main(String[] args) {
+        //komentar
+        int godinaRodjenjaStudenta;
+        int godinaRodjenjaProfesora;
+
+        godinaRodjenjaStudenta = 1998;
+        godinaRodjenjaProfesora = 1980;
+
+        // double pi = 3.1415;
+
+        // boolean daLiJeStudentPolozioIspit = true;
+
+        // char slovo = 'M';
+
+        // String imeStudenta;
+        // String prezimeStudenta;
+
+        // imeStudenta = "Petar";
+        // prezimeStudenta = "Jovanovic";
+
+        int razlikaUGodinama = godinaRodjenjaStudenta - godinaRodjenjaProfesora;
+
+        System.out.println(razlikaUGodinama);
+
+    }
+}
