c05dd3478f5572255499dba07c53598ae2afa5fd
unknown <nedovic.vuk@gmail.com>
2023-07-23 15:28:59 +0200

    basic operations and messing with numbers

---
+++ prva vezba/operacije.java
@@ -0,0 +1,18 @@
+public class operacije {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 3;
+        int c = a * b;
+
+        System.out.println(c);
+
+        c = a * a;
+
+        System.out.println(c);
+
+        double d = 1.0 * a / b;
+
+        System.out.println(d);
+    }
+}
