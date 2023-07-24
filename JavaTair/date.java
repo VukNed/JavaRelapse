b06d83189315b54a58584aff74aa9e61449c86f8
unknown <nedovic.vuk@gmail.com>
2023-07-24 23:02:37 +0200

    Testing Date and Time Classes

---
+++ JavaTair/date.java
@@ -0,0 +1,8 @@
+import java.time.LocalTime;
+//localTime package was auto added
+public class date {
+    public static void main(String[] args) {
+        LocalTime trenutnoVreme = LocalTime.now();
+        System.out.println(trenutnoVreme);
+    }
+}
