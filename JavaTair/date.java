5afd3379fee5dc7a9a3cd7f43420af4a4931b6a2
VukNed <nedovic.vuk@gmail.com>
2023-07-24 23:21:47 +0200

    Resolved the bug, there was a typo

--- JavaTair/date.java
+++ JavaTair/date.java
@@ -1,8 +1,12 @@
+import java.time.LocalDate;
 import java.time.LocalTime;
-//localTime package was auto added
+//localTime and Date package were auto added
 public class date {
     public static void main(String[] args) {
         LocalTime trenutnoVreme = LocalTime.now();
         System.out.println(trenutnoVreme);
+        //i forgot () after now
+        LocalDate trenutniDatum = LocalDate.now();
+        System.out.println(trenutniDatum);
     }
 }
