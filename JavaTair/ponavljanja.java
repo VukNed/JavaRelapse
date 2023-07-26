f59bd6e0fb2dc301c7d7f61d7d577fb7e4aa9398
VukNed <nedovic.vuk@gmail.com>
2023-07-26 12:02:35 +0200

    common for loop

---
+++ JavaTair/ponavljanja.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ponavljanja {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        for ( int i=0; i <10; i++ ) {
+            int b= i+1;
+            System.out.println(b);
+        }
+
+        System.out.println("Program je zavrsen!");
+
+    }
+}
