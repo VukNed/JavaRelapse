279d3f2a4f917a05c4abae82082d88ff796432bc
VukNed <nedovic.vuk@gmail.com>
2023-07-26 20:31:22 +0200

    Made a basic calculator using Switc and Case

---
+++ JavaTair/calculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Input first number");
+        int a = s.nextInt();
+
+        System.out.println("Input second number");
+        int b = s.nextInt();
+
+        System.out.println("Choose your fighter + - * / ");
+        char operation = s.next().charAt(0);
+        
+        switch (operation) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+        }
+        // for testing the scanner
+        // System.out.println(operation);
+        // System.out.println(a+ ", " +b);
+
+    }
+}
