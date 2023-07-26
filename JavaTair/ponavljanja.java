ae7264b0a6f0daac245a7b74101059369cec403a
VukNed <nedovic.vuk@gmail.com>
2023-07-26 12:08:50 +0200

    Added if/else to recursion to test if the number is even or odd

--- JavaTair/ponavljanja.java
+++ JavaTair/ponavljanja.java
@@ -6,8 +6,15 @@ public class ponavljanja {
         Scanner s = new Scanner(System.in);
 
         for ( int i=0; i <10; i++ ) {
-            int b= i+1;
-            System.out.println(b);
+            System.out.println("Unesite neki broj");
+            int broj = s.nextInt();
+
+            if (broj%2 == 0) {
+                System.out.println("Uneli ste paran broj");
+            } else {
+                System.out.println("Uneli ste neparan broj");
+            }
+
         }
 
         System.out.println("Program je zavrsen!");
