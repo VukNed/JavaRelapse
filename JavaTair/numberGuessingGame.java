877ec5eb5db4410f19835531fa5bd6e6d4ed53d4
VukNed <nedovic.vuk@gmail.com>
2023-07-26 12:29:27 +0200

    Made a number guessing game without following the tutorial, i still want to add a way for user to know how many guesses he has left, and a message that will tell them when they run out of guesses

---
+++ JavaTair/numberGuessingGame.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class numberGuessingGame {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        Random random = new Random();
+
+        System.out.println("You have 5 guesses to find a number from 1 to 100");
+
+        int randomInt= random.nextInt(100);
+
+        //this sout was used to check if randomInt is working
+        //System.out.println(randomInt);
+
+        for (int i=0; i < 5; i++) {
+            System.out.println("Make your guess");
+            int guess = s.nextInt();
+
+            if (guess < randomInt) {
+                System.out.println("Aim higher");
+            } else {
+                if (guess > randomInt) {
+                System.out.println("Aim lower");
+                } else {
+                System.out.println("Thats the number!");
+            }
+            }
+
+        }
+
+    }
+}
