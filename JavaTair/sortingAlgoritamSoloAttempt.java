c77b35ed1aa2dd3761b4d6ee7a5e2e9b16bd58da
VukNed <nedovic.vuk@gmail.com>
2023-08-01 19:39:54 +0200

    for some reason .lenght didnt work

--- JavaTair/sortingAlgoritamSoloAttempt.java
+++ JavaTair/sortingAlgoritamSoloAttempt.java
@@ -9,20 +9,33 @@ public class sortingAlgoritamSoloAttempt {
         int rec = s.nextInt();
         int[] array = new int[rec];
 
+        // filling in the array
         for(int i=0; i < rec; i++){
 
             System.out.println("Write a number");
             array[i] = s.nextInt();
         }
 
-        System.out.println("Members of array are: ");
-        for (int members: array) {
-            System.out.print(members + " ");
+        // sorting algorithm
+        for (int k=0; k<array.lenght; k++){
+            for (int l=0; l<array.lenght; l++){
+
+                int temp = 0;
+                if (array[k] > array [l]) {
+                    temp = array[k];
+                    array[k] = array[l];
+                    array[l] = temp;
+                }
+
+            }
+
+            System.out.println(array[k]);
+
         }
+        
 
-
-
-        // System.out.println(min);
-
+        // System.out.println("Members of array are: ");
+        // for (int members: array) {
+        //     System.out.print(members + " ");
+        }
     }
-}
