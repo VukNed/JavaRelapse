7aabb32c4d2ed5bdb72b1b88f23b413642b09ff8
VukNed <nedovic.vuk@gmail.com>
2023-08-28 17:05:50 +0200

    number guessing game

---
+++ Algoritmi/Cas1/cas1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class cas1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        Random random = new Random();
+
+        System.out.println("You have 5 guesses to find a number from 1 to 100");
+
+        int randomInt= random.nextInt(100);
+
+        //this sout was used to check if randomInt is working
+        //System.out.println(randomInt);
+
+        for (int i=0; i < 5; i++) {
+            System.out.println("Make your guess");
+            int guess = s.nextInt();
+
+            //it has to begin with 4, because the 1st value for i is 0
+            int trial = 4 - i;
+
+            if (guess < randomInt) {
+                System.out.println("Aim higher, you have " + trial + " guesses left");
+            } else {
+                if (guess > randomInt) {
+                System.out.println("Aim lower, you have " + trial + " guesses left");
+                } else {
+                System.out.println("Thats the number!");
+                break;
+                }
+            }
+                // if i use single = java reads it as boolean
+                if (trial == 0) {
+                    System.out.println("Game Over");
+                }
+
+        }
+
+    }
+}
