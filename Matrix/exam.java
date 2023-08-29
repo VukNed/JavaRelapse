6a43872cbbe6fd912ae7d9a92757cf136952113e
VukNed <nedovic.vuk@gmail.com>
2023-08-29 21:42:24 +0200

    Tried exam from last semester where array is multyplied by matrix and written in new matrix

---
+++ Matrix/exam.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class exam {
+    
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Array lenght?");
+        int l = s.nextInt();
+
+        System.out.println("Rows in Matrix?");
+        int rows = s.nextInt();
+
+        System.out.println("Columns in matrix?");
+        int cols = s.nextInt();
+
+        int arr[] = new int[l];
+
+        System.out.println("Fill in the array");
+        for(int i=0; i<l; i++){
+            arr[i] = s.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+
+        int matrix[][] = new int[rows][cols];
+        int result[][] = new int[rows][cols];
+
+        System.out.println("Fill in the Matrix");
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                matrix[i][j]=s.nextInt();
+            }
+        }
+
+        System.out.println("Your Matrix");
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                System.out.print(matrix[i][j] + "\t");;
+            }
+            System.out.println();
+        }
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                    result[i][j] += matrix[i][j] * arr[j];
+
+            }
+        }
+        System.out.println("Your Result");
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                System.out.print(result[i][j] + "\t");;
+            }
+            System.out.println();
+        }
+
+
+
+    }
+
+}
